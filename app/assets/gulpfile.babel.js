00f8421b80b68421f88f26845d099b30596cceee
oliver <oliver@pop-os.localdomain>
2021-07-24 13:57:37 +0800

    Revert "configure gulp to import css from node_modules"
    This reverts commit 7c520e2ce6ddb244ad2520a4589154ba1ab1cf81.

--- app/assets/gulpfile.babel.js
+++ app/assets/gulpfile.babel.js
@@ -5,7 +5,6 @@ import cleanCSS from 'gulp-clean-css';
 import gulpIf from 'gulp-if';
 import sourceMaps from 'gulp-sourcemaps';
 const sass = require('gulp-sass')(require('sass'));
-import packageImporter from 'node-sass-package-importer';
 
 const PRODUCTION = yargs.argv.prod;
 
@@ -14,33 +13,15 @@ const paths = {
     src: ['src/scss/bundle.scss'],
     dest: ['dist/css'],
   },
-  materializeCss: {
-    src: ['./node_modules/materialize-css/**/*.min.css'],
-    dest: ['dist/css/materialize-css'],
-  },
-  normalizeCss: {
-    src: ['./node_modules/normalize.css/**/*.css'],
-    dest: ['dist/css/normalize.css'],
-  },
 };
 
 export const clean = () => del(['dist']);
-
-const importNormalize = () =>
-  gulp.src(paths.normalizeCss.src).pipe(gulp.dest(paths.normalizeCss.dest));
-
-const importMaterialize = () =>
-  gulp.src(paths.materializeCss.src).pipe(gulp.dest(paths.materializeCss.dest));
 
 export const styles = () => {
   return gulp
     .src(paths.styles.src)
     .pipe(gulpIf(!PRODUCTION, sourceMaps.init()))
-    .pipe(
-      sass({
-        includePaths: ['node_modules'],
-      }).on('error', sass.logError)
-    )
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulpIf(PRODUCTION, cleanCSS({ compatibility: 'ie8' })))
     .pipe(gulpIf(!PRODUCTION, sourceMaps.write()))
     .pipe(gulp.dest(paths.styles.dest));
@@ -50,8 +31,4 @@ export const watch = () => {
   gulp.watch('src/scss/**/*.scss', styles);
 };
 
-export const dev = gulp.series(
-  clean,
-  gulp.parallel([importNormalize, importMaterialize, styles]),
-  watch
-);
+export const dev = gulp.series(clean, gulp.parallel(styles), watch);
